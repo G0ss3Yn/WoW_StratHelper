9631aeecf51e23702c89f50b5ab49fde71e17abf
G0ss3Yn <G0ss3Yn@hotmail.com>
2022-12-21 03:33:29 +0100

    Add wod strat file (terres sacrées d'ombrelune)

---
+++ strats_dj/wod.lua
@@ -0,0 +1,12 @@
+local AddOnName, this = ... ;
+
+--[[ Terres sacrées d'ombrelune --]]
+--[[ Sadana Rougefurie --]]
+this.stratDB["75509"] = {"Pendant Sombre éclipse, se mettre dans les lunes rondes (sur le cercle intérieur)", "Tuer l'add rapidement", "Rester à l'écart des zones violettes et des dagues plantées"} ;
+--[[ Nhallish --]]
+this.stratDB["75829"] = {"Ne pas tanker vers le groupe", "Eviter les AoE/runes au sol", "Si Add pop, le tank le récupère", "Si vous êtes transféré dans un autre monde, tuer son double et on clicker dessus pour revenir"} ;
+--[[ Ossegueule --]]
+this.stratDB["75452"] = {"Ne pas tanker vers le groupe", "Eviter les plaquage (gros frontal qui bump)", "Si bump dans l'eau, revenir via les geysers", "Se mettre dans les zones au sol pour ne pas se faire inhaler", "Le reste du temps, ne pas rester dedans", "Ignorer les deux adds mais éviter leurs plaquages"} ;
+--[[ Ner'zhul --]]
+this.stratDB["76407"] = {"Eviter Malice (cône au sol)", "Ne pas rester près des zones violettes qui pulsent", "Quand les squelettes pop, focus sur un seul pour pouvoir passer", "Eviter les AoE au sol"}
+
